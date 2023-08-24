272c66310e37bed3d80934531719fd128e11c61b
Micro Social <myomin@blue-stone.net>
2023-08-25 00:28:23 +0630

    mobile screen err

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import {GameService} from "./service/game.service";
 import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [GameService],
   bootstrap: [AppComponent]
